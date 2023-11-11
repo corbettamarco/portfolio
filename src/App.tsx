5d3482632b6fd5c450b0e2c4dda6ed2f9b862f9a
corbettamarco-huroos <m.corbetta@huroos.com>
2023-11-11 23:09:38 +0100

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { AppShell } from "@saas-ui/react";
-import { AboveTheFold } from "./components/aboveTheFold/AboveTheFold";
 import { NavBar } from "./components/NavBar";
-import { CustomSidebar } from "./components/sidebarComponents/CustomSidebar";
-import { Footer } from "./components/footer/Footer";
-import { ExperienceSection } from "./components/experienceSection/ExperienceSection";
-import { Projects } from "./components/projectsSection/Projects";
+import { AboveTheFold } from "./components/aboveTheFold/AboveTheFold";
 import Contact from "./components/contactSection/Contact";
-import { Services } from "./components/servicesSection/Services";
+import { ExperienceSection } from "./components/experienceSection/ExperienceSection";
+import { Footer } from "./components/footer/Footer";
+import { Projects } from "./components/projectsSection/Projects";
+import { CustomSidebar } from "./components/sidebarComponents/CustomSidebar";
 export const App = () => {
   return (
     <AppShell navbar={<NavBar />} sidebar={<CustomSidebar />}>
